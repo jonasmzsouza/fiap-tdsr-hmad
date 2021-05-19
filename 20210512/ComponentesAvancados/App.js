e088c2ccdfd4063f6c4665946bafca0f23c28f38
Jonas Muniz <jonasmzsouza@gmail.com>
2021-05-19 17:57:39 -0300

    Aula 19 - Componentes Avancaçadpos: ScrollView

--- 20210512/ComponentesAvancados/App.js
+++ 20210512/ComponentesAvancados/App.js
@@ -1,17 +1,42 @@
 import React from 'react'
 import {
-  ActivityIndicator,
-  SafeAreaView
+  Image,
+  SafeAreaView,
+  ScrollView
 } from 'react-native'
 
 class App extends React.Component {
   render() {
     return (
       <SafeAreaView>
-        <ActivityIndicator
-          animating={ true }
-          color='#F00'
-          size={ 80 } />
+        <ScrollView 
+          onScrollBeginDrag={ ()=> alert('Usuário iniciou o scroll')}
+          horizontal={ true }>
+          <Image 
+            resizeMode='cover'
+            source={{ uri: 'https://picsum.photos/id/10/200/300' }}
+            style={{ height : 200, width : 400 }} />
+
+          <Image 
+            resizeMode='cover'
+            source={{ uri: 'https://picsum.photos/id/20/200/300' }}
+            style={{ height : 200, width : 400 }} />
+
+          <Image 
+            resizeMode='cover'
+            source={{ uri: 'https://picsum.photos/id/30/200/300' }}
+            style={{ height : 200, width : 400 }} />
+
+          <Image 
+            resizeMode='cover'
+            source={{ uri: 'https://picsum.photos/id/40/200/300' }}
+            style={{ height : 200, width : 400 }} />
+
+          <Image 
+            resizeMode='cover'
+            source={{ uri: 'https://picsum.photos/id/50/200/300' }}
+            style={{ height : 200, width : 400 }} />                                                
+        </ScrollView>
       </SafeAreaView>
     )
   }
