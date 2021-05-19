605a78d4d00e39a504d2e5a33394d5123aedae5a
Jonas Muniz <jonasmzsouza@gmail.com>
2021-05-19 18:06:24 -0300

    Aula 19 - Componentes Avancaçadpos: KeyboardAvoidingView

--- 20210512/ComponentesAvancados/App.js
+++ 20210512/ComponentesAvancados/App.js
@@ -1,43 +1,27 @@
 import React from 'react'
 import {
   Image,
+  KeyboardAvoidingView,
   SafeAreaView,
-  ScrollView
+  TextInput,
+  View
 } from 'react-native'
 
 class App extends React.Component {
   render() {
     return (
-      <SafeAreaView>
-        <ScrollView 
-          onScrollBeginDrag={ ()=> alert('Usuário iniciou o scroll')}
-          horizontal={ true }>
+      <KeyboardAvoidingView style={{ flex : 1 }}>
+        <View style={{ flex: 1, justifyContent : 'center', padding : 16 }}>
           <Image 
-            resizeMode='cover'
-            source={{ uri: 'https://picsum.photos/id/10/200/300' }}
-            style={{ height : 200, width : 400 }} />
-
-          <Image 
-            resizeMode='cover'
-            source={{ uri: 'https://picsum.photos/id/20/200/300' }}
-            style={{ height : 200, width : 400 }} />
-
-          <Image 
-            resizeMode='cover'
-            source={{ uri: 'https://picsum.photos/id/30/200/300' }}
-            style={{ height : 200, width : 400 }} />
-
-          <Image 
-            resizeMode='cover'
-            source={{ uri: 'https://picsum.photos/id/40/200/300' }}
-            style={{ height : 200, width : 400 }} />
-
-          <Image 
-            resizeMode='cover'
-            source={{ uri: 'https://picsum.photos/id/50/200/300' }}
-            style={{ height : 200, width : 400 }} />                                                
-        </ScrollView>
-      </SafeAreaView>
+              resizeMode='cover'
+              source={{ uri: 'https://picsum.photos/id/70/200/300' }}
+              style={{ height : 200, width : 400 }} />
+            
+            <TextInput style={{ borderWidth : 1, marginTop : 8 }} />
+            <TextInput style={{ borderWidth : 1, marginTop : 8 }} />
+            <TextInput style={{ borderWidth : 1, marginTop : 8 }} />
+        </View>
+      </KeyboardAvoidingView>
     )
   }
 }
