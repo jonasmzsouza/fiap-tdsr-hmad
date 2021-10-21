730b869d259f094490492fb09d9e8f7e33de705c
Jonas Souza <jonasmzsouza@gmail.com>
2021-10-21 01:51:04 -0300

    Aula 36 - Publicação de App

---
+++ 20211020/exemplodepublicacao/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Exemplo de Publicação!</Text>
+    </View>
+  )
+}
+
+export default App
